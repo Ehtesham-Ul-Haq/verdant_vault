7eb9338a46f04a565fca5dc48b926b6ee267c7f6
Ehtesham Ul Haq <ihtishamb7@gmail.com>
2025-09-14 03:37:14 +0500

    brand color tested

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="h1">
+      <h1 className="h1 text-brand">
         Verdant Vault - The new way to backup your files.
       </h1>
     </div>
